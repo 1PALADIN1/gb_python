b508a48ffb053c5fd0bdcc40546cf8d110a1ea9e
ruslan <sonikx94@yandex.ru>
2022-09-07 20:55:34 +0300

    Задание 2

--- homework1/homework.py
+++ homework1/homework.py
@@ -5,7 +5,11 @@
 # - 7 -> да
 # - 1 -> нет
 def task1():
-    num = int(input())
+    num = int(input("Введите день недели:\n"))
+    if num > 7 or num < 1:
+        print("некорректный день недели")
+        return
+
     if num == 6 or num == 7:
         print("да")
         return
@@ -13,4 +17,27 @@ def task1():
     print("нет")
 
 
-task1()
+# 2. Напишите программу для проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+def task2():
+    for i in range(8):
+        x = 1 if check_bitmask(i, 0) else 0
+        y = 1 if check_bitmask(i, 1) else 0
+        z = 1 if check_bitmask(i, 2) else 0
+
+        result = "да" if validate_expr(x, y, z) else "нет"
+        print("Утверждение истинно для набора (" + str(x) + ", " + str(y) + ", " + str(z) + ")? " + result)
+
+
+def check_bitmask(num: int, bit_order: int) -> bool:
+    mask = 1 << bit_order
+    return (num & mask) == mask
+
+
+def validate_expr(x: int, y: int, z: int) -> bool:
+    return not (x or y or z) == (not x and not y and not z)
+
+
+# task1()
+task2()
+
