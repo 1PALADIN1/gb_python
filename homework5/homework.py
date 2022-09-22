adca7e24690b391653946ef545530715af80095f
ruslan <sonikx94@yandex.ru>
2022-09-22 18:45:40 +0300

    Задание 2 без ИИ

--- homework5/homework.py
+++ homework5/homework.py
@@ -1,3 +1,9 @@
+import random
+import os
+
+max_take_candies = 28
+
+
 # 1. Напишите программу, удаляющую из текста все слова, содержащие "абв".
 def task1():
     result = []
@@ -22,15 +28,57 @@ def task1():
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота "интеллектом"
 def task2():
-    pass
+    candies = 2021
+    turn_num = 1
+    whose_turn = random.choice([1, 2])
+    print("Начинает игрок", whose_turn)
+
+    while candies > 0:
+        print("============== ХОД", turn_num, "==============")
+        take_candies = candy_player_turn(whose_turn, candies)
+
+        candies -= take_candies
+
+        if candies > 0:
+            turn_num += 1
+            whose_turn = 1 if whose_turn == 2 else 2
+            cls()
+
+    print("Игра окончена! Победил игрок", whose_turn)
 
 
-# Создайте программу для игры в "Крестики-нолики".
+def candy_player_turn(player: int, candies_left: int) -> int:
+    print("Ход игрока", player, "\nОсталось конфет:", candies_left)
+
+    result = 0
+    while True:
+        max_candies = max_take_candies if max_take_candies < candies_left else candies_left
+        result = int(input("Введите количество конфет (MAX: " + str(max_candies) + "):\n"))\
+
+        if result > max_candies:
+            print("Количество конфет не должно превышать " + str(max_candies) + "!")
+            continue
+
+        if result <= 0:
+            print("Количество конфет должно быть больше 0!")
+            continue
+
+        break
+
+    return result
+
+
+def cls():
+    # чтобы очистка заработала, нужно установить галочку в конфигурации "Emulate terminal in output console"
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# 3. Создайте программу для игры в "Крестики-нолики".
 def task3():
     pass
 
 
-# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Пример:
 # Введите текст для кодировки: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
 # Текст после кодировки: 12W1B12W3B24W1B14W
@@ -39,7 +87,7 @@ def task4():
     pass
 
 
-task1()
+# task1()
 task2()
 task3()
 task4()
