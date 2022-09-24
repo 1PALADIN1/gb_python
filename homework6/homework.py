7ef466e29a85872c50cc9c679a9ef8ea71562bc5
ruslan <sonikx94@yandex.ru>
2022-09-24 16:02:27 +0300

    Задание 2

--- homework6/homework.py
+++ homework6/homework.py
@@ -34,8 +34,24 @@ def calc_tails(input_str: str) -> int:
 # применив лямбды, filter, map, zip, enumerate, списочные выражения.
 def task2():
     print("Задание 2")
-    pass
+    # Задание 2 из домашней работы 3. Напишите программу, которая найдёт произведение пар чисел списка.
+    # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+    # Пример:
+    # [2, 3, 4, 5, 6] => [12, 15, 16];
+    # [2, 3, 5, 6] => [12, 15]
+    input_string = input("Введите последовательность чисел через пробел (пример, 2 3 4 5 6):\n")
+    ls = [int(i) for i in input_string.split()]
+
+    ls_reversed = ls.copy()
+    ls_reversed.reverse()
+
+    size = len(ls)
+    mid = int(size / 2) if size % 2 == 0 else int(size / 2) + 1
+
+    print(
+        list(map(lambda x: x[0] * x[1], list(zip(ls[:mid], ls_reversed[:mid]))))
+    )
 
 
 task1()
-# task2()
+task2()
